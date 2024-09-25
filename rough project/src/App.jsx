0449f46475a74002440da1582620e5861f7abe57
SyedFazal110 <syedfazal808@gmail.com>
2024-09-25 10:21:47 +0500

    Practice

---
+++ rough project/src/App.jsx
@@ -0,0 +1,41 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Card from './component/Card'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     <h1>Counter {count}</h1>
+//       <button onClick={( ) =>setCount( count +1)}> Add</button>
+//       <button onClick={() => setCount(count - 1)}>less</button>
+
+//       {count >= 5 ? <Card title="card" description="card description"/> : <p>Small</p>}
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+// const [age , setAge] = useState(0)
+
+
+//   return (
+//     <>
+//     <h1>Age {age}</h1>
+//     <button onClick={( ) =>setAge( age +1)}> Add</button>
+//     <button onClick={() => setAge( age -1)}>less</button>
+//     <div>{age<10?<p>nhi kar skte</p>:age <=18 ?<p>100 zyda do </p>:<p>Kar skte ho</p>}</div>
+//     </>
+//   )
+// }
+
+export default App
